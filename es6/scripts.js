cbca9c2e85b4a7730231c45b1b9b150c7ad41dac
pjliddy <pjliddy@gmail.com>
2017-07-22 10:58:16 -0400

    Added babel for es6 transpiling
    Working JS files are in es6/ directory. They get transpiled on save to
    the js/ directory. No links need to be updated in working files

---
+++ es6/scripts.js
@@ -0,0 +1,283 @@
+'use strict'
+
+// set path to Google Sheet with listing data (JSON feed)
+const listingPath = "https://spreadsheets.google.com/feeds/list/1hzXzXdK1fqgaZC_eQdxLRFkUOHlW7puy9w9CPrNl5uE/od6/public/values?alt=json"
+
+// initialize global listing data object
+let listingsObj = { }
+
+// global listing data object constructor
+
+const ListingData = function ( ) {
+   data: [ ]
+ }
+
+// initialize data from Google Sheets JSON feed
+
+ListingData.prototype.setData = function (feedData) {
+  // initialize data array
+  this.data = []
+  // iterate through listing objects in JSON data feed
+  feedData.forEach(obj => {
+    // create an empty object for each listing
+    let newObj = { }
+    // iterate through properties of google sheet JSON feed
+    for (let prop in obj) {
+      // only save name:value pairs beginning with 'gsx$'
+      if (prop.startsWith('gsx$')) {
+        // slice off the first 4 chars of the property name
+        const newProp = prop.slice(4)
+        // and take the value of $t for that property
+        newObj[newProp] = obj[prop].$t
+        // convert list of image names into an array
+        if (newProp === 'images') {
+          let arr = newObj[newProp].split(',')
+          // remove whitespace before or after comma delimiter
+          arr.forEach((e, i, a) => {
+            a[i] = e.trim()
+          })
+          newObj[newProp] = arr
+        }
+      }
+    }
+    // push the new data object onto the listing data array
+    this.data.push(newObj)
+  })
+}
+
+//
+// Template Rendering Functions
+//
+
+// render template and return html content
+
+function renderTemplate(source, data) {
+  const template  = Handlebars.compile(source)
+  const content = template({data})
+  return content
+}
+
+// render template and insert in target element
+
+function insertTemplate(target, source, data) {
+  const template  = Handlebars.compile(source)
+  const content = template({data})
+  $(target).html(content)
+}
+
+// render template and append to target element
+
+function appendTemplate(target, source, data) {
+  const template  = Handlebars.compile(source)
+  const content = template({data})
+  $(target).append(content)
+}
+
+// display modal
+
+function showModal(content) {
+  // if there's already a modal
+  if ($('.modal').length) {
+    // replace the existing modal
+    insertTemplate($('.modal')[0], content)
+  } else {
+    // append a new modal to the body
+    $('body').append(content)
+  }
+  // display modal
+  $('.modal').modal('show')
+
+}
+//
+// AJAX calls
+//
+
+// make AJAX call to JSON feed and return promise
+
+function getListingData(path) {
+  return $.ajax({
+    url: path,
+    cache: true
+  })
+}
+
+// make AJAX call to Handlebars template file and return promise
+
+function getTemplate (path) {
+  return $.ajax({
+    url: path,
+    cache: true
+  })
+}
+
+//  Submit Contact Form
+
+function submitContact (data) {
+  return $.ajax({
+    url: "./cgi/submit.php",
+    type: "POST",
+    data: data,
+    cache: false
+  })
+}
+
+//
+//  Listing Detail Functions
+//
+
+// get data from listingData array from mls number
+
+function getDetailData (mls) {
+  // return individual data object from listingData with mls number
+  const result = listingsObj.data.find(
+    function (e) {
+      return e.mls == mls
+    }
+  )
+  return result
+}
+
+// render and display listing detail in a full screen modal
+
+function showDetails (mls) {
+  // getModalTemplate('body','js/templates/detail-modal.hbs', mls);
+  getTemplate('js/templates/modal-detail.hbs')
+    .then((template) => {
+      const data = getDetailData(mls)
+      const content = renderTemplate(template, data)
+      showModal(content)
+    })
+    .fail((err) => console.log('listing template is not available'))
+}
+
+
+
+// jQuery Gallery Setup
+
+function setUpGallery() {
+  // $('.flexslider').flexslider({
+  //   animation: "slide"
+  // })
+
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  })
+
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  })
+}
+
+//
+//  Contact Submission Functions
+//
+
+function showSubmitError () {
+  getTemplate('js/templates/modal-submit-error.hbs')
+    .then((template) => {
+      const content = renderTemplate(template)
+      showModal(content)
+    })
+    .fail((err) => console.log('submit errror template is not available'))
+}
+
+function showSubmitSuccess () {
+  getTemplate('js/templates/modal-submit-success.hbs')
+    .then((template) => {
+      const content = renderTemplate(template)
+      showModal(content)
+    })
+    .fail((err) => console.log('submit success template is not available'))
+}
+//
+//  Event Handlers
+//
+
+function handleEvents () {
+  // jQuery for page scrolling feature using jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+      const $anchor = $(this)
+      $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      }, 800, 'easeInOutExpo')
+      event.preventDefault()
+  });
+
+  // Highlight the top nav as page scrolls
+  $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
+  })
+
+  // Close the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function(){
+    $('.navbar-toggle:visible').click()
+  })
+
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
+  // show listing detail modal when "see more" is clicked
+  $('.listings').on('click', '.see-more', (e) => {
+    showDetails($(e.target).data('mls'))
+  })
+
+  // submit contact form
+  $('#contact-form').submit(event => {
+    event.preventDefault()
+    const email = $("input#email").val()
+    const data = {
+      email: email
+    }
+    submitContact(data)
+      .then(showSubmitSuccess)
+      .fail(showSubmitError)
+
+    $("input#email").val('')
+  })
+
+  // initialize jquery gallery in detail modal
+  $('body').on('shown.bs.modal', '#detail-modal', () => {
+    setUpGallery()
+  })
+}
+
+//
+// Document Ready
+//
+
+$(function() {
+  // set event handlers
+  handleEvents()
+
+  // get data from JSON feed and wait for promise to be returned
+  getListingData(listingPath)
+    .then((data) => {
+      // set global listing object to JSON feed data
+      listingsObj = new ListingData()
+
+      listingsObj.setData(data.feed.entry)
+
+      // get template and render
+      getTemplate('js/templates/listings.hbs')
+        .then((template) => {
+          insertTemplate('#listings', template, listingsObj.data)
+        })
+        .fail((err) => console.log('listing template is not available'))
+    })
+    .fail((err) => console.log('data feed is not available'))
+})
--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,52 @@
-'use strict'
+'use strict';
 
 // set path to Google Sheet with listing data (JSON feed)
-const listingPath = "https://spreadsheets.google.com/feeds/list/1hzXzXdK1fqgaZC_eQdxLRFkUOHlW7puy9w9CPrNl5uE/od6/public/values?alt=json"
+
+var listingPath = "https://spreadsheets.google.com/feeds/list/1hzXzXdK1fqgaZC_eQdxLRFkUOHlW7puy9w9CPrNl5uE/od6/public/values?alt=json";
 
 // initialize global listing data object
-let listingsObj ={}
+var listingsObj = {};
 
 // global listing data object constructor
 
-const ListingData = function ( ) {
-   data: []
- }
+var ListingData = function ListingData() {
+  data: [];
+};
 
 // initialize data from Google Sheets JSON feed
 
 ListingData.prototype.setData = function (feedData) {
+  var _this = this;
+
   // initialize data array
-  this.data = []
+  this.data = [];
   // iterate through listing objects in JSON data feed
-  feedData.forEach(obj => {
+  feedData.forEach(function (obj) {
     // create an empty object for each listing
-    let newObj = {}
+    var newObj = {};
     // iterate through properties of google sheet JSON feed
-    for (let prop in obj) {
+    for (var prop in obj) {
       // only save name:value pairs beginning with 'gsx$'
       if (prop.startsWith('gsx$')) {
         // slice off the first 4 chars of the property name
-        const newProp = prop.slice(4)
+        var newProp = prop.slice(4);
         // and take the value of $t for that property
-        newObj[newProp] = obj[prop].$t
+        newObj[newProp] = obj[prop].$t;
         // convert list of image names into an array
         if (newProp === 'images') {
-          let arr = newObj[newProp].split(',')
+          var arr = newObj[newProp].split(',');
           // remove whitespace before or after comma delimiter
-          arr.forEach((e, i, a) => {
-            a[i] = e.trim()
-          })
-          newObj[newProp] = arr
+          arr.forEach(function (e, i, a) {
+            a[i] = e.trim();
+          });
+          newObj[newProp] = arr;
         }
       }
     }
     // push the new data object onto the listing data array
-    this.data.push(newObj)
-  })
-}
+    _this.data.push(newObj);
+  });
+};
 
 //
 // Template Rendering Functions
@@ -52,25 +55,25 @@ ListingData.prototype.setData = function (feedData) {
 // render template and return html content
 
 function renderTemplate(source, data) {
-  const template  = Handlebars.compile(source)
-  const content = template({data})
-  return content
+  var template = Handlebars.compile(source);
+  var content = template({ data: data });
+  return content;
 }
 
 // render template and insert in target element
 
 function insertTemplate(target, source, data) {
-  const template  = Handlebars.compile(source)
-  const content = template({data})
-  $(target).html(content)
+  var template = Handlebars.compile(source);
+  var content = template({ data: data });
+  $(target).html(content);
 }
 
 // render template and append to target element
 
 function appendTemplate(target, source, data) {
-  const template  = Handlebars.compile(source)
-  const content = template({data})
-  $(target).append(content)
+  var template = Handlebars.compile(source);
+  var content = template({ data: data });
+  $(target).append(content);
 }
 
 // display modal
@@ -79,14 +82,13 @@ function showModal(content) {
   // if there's already a modal
   if ($('.modal').length) {
     // replace the existing modal
-    insertTemplate($('.modal')[0], content)
+    insertTemplate($('.modal')[0], content);
   } else {
     // append a new modal to the body
-    $('body').append(content)
+    $('body').append(content);
   }
   // display modal
-  $('.modal').modal('show')
-
+  $('.modal').modal('show');
 }
 //
 // AJAX calls
@@ -98,27 +100,27 @@ function getListingData(path) {
   return $.ajax({
     url: path,
     cache: true
-  })
+  });
 }
 
 // make AJAX call to Handlebars template file and return promise
 
-function getTemplate (path) {
+function getTemplate(path) {
   return $.ajax({
     url: path,
     cache: true
-  })
+  });
 }
 
 //  Submit Contact Form
 
-function submitContact (data) {
+function submitContact(data) {
   return $.ajax({
     url: "./cgi/submit.php",
     type: "POST",
     data: data,
     cache: false
-  })
+  });
 }
 
 //
@@ -127,30 +129,26 @@ function submitContact (data) {
 
 // get data from listingData array from mls number
 
-function getDetailData (mls) {
+function getDetailData(mls) {
   // return individual data object from listingData with mls number
-  const result = listingsObj.data.find(
-    function (e) {
-      return e.mls == mls
-    }
-  )
-  return result
+  var result = listingsObj.data.find(function (e) {
+    return e.mls == mls;
+  });
+  return result;
 }
 
 // render and display listing detail in a full screen modal
 
-function showDetails (mls) {
+function showDetails(mls) {
   // getModalTemplate('body','js/templates/detail-modal.hbs', mls);
-  getTemplate('js/templates/modal-detail.hbs')
-    .then((template) => {
-      const data = getDetailData(mls)
-      const content = renderTemplate(template, data)
-      showModal(content)
-    })
-    .fail((err) => console.log('listing template is not available'))
+  getTemplate('js/templates/modal-detail.hbs').then(function (template) {
+    var data = getDetailData(mls);
+    var content = renderTemplate(template, data);
+    showModal(content);
+  }).fail(function (err) {
+    return console.log('listing template is not available');
+  });
 }
-
-
 
 // jQuery Gallery Setup
 
@@ -167,7 +165,7 @@ function setUpGallery() {
     itemWidth: 210,
     itemMargin: 5,
     asNavFor: '#slider'
-  })
+  });
 
   $('#slider').flexslider({
     animation: "slide",
@@ -175,109 +173,108 @@ function setUpGallery() {
     animationLoop: false,
     slideshow: false,
     sync: "#carousel"
-  })
+  });
 }
 
 //
 //  Contact Submission Functions
 //
 
-function showSubmitError () {
-  getTemplate('js/templates/modal-submit-error.hbs')
-    .then((template) => {
-      const content = renderTemplate(template)
-      showModal(content)
-    })
-    .fail((err) => console.log('submit errror template is not available'))
+function showSubmitError() {
+  getTemplate('js/templates/modal-submit-error.hbs').then(function (template) {
+    var content = renderTemplate(template);
+    showModal(content);
+  }).fail(function (err) {
+    return console.log('submit errror template is not available');
+  });
 }
 
-function showSubmitSuccess () {
-  getTemplate('js/templates/modal-submit-success.hbs')
-    .then((template) => {
-      const content = renderTemplate(template)
-      showModal(content)
-    })
-    .fail((err) => console.log('submit success template is not available'))
+function showSubmitSuccess() {
+  getTemplate('js/templates/modal-submit-success.hbs').then(function (template) {
+    var content = renderTemplate(template);
+    showModal(content);
+  }).fail(function (err) {
+    return console.log('submit success template is not available');
+  });
 }
 //
 //  Event Handlers
 //
 
-function handleEvents () {
+function handleEvents() {
   // jQuery for page scrolling feature using jQuery Easing plugin
-  $('a.page-scroll').bind('click', function(event) {
-      const $anchor = $(this)
-      $('html, body').stop().animate({
-          scrollTop: ($($anchor.attr('href')).offset().top - 50)
-      }, 800, 'easeInOutExpo')
-      event.preventDefault()
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 50
+    }, 800, 'easeInOutExpo');
+    event.preventDefault();
   });
 
   // Highlight the top nav as page scrolls
   $('body').scrollspy({
-      target: '.navbar-fixed-top',
-      offset: 51
-  })
+    target: '.navbar-fixed-top',
+    offset: 51
+  });
 
   // Close the Responsive Menu on Menu Item Click
-  $('.navbar-collapse ul li a').click(function(){
-    $('.navbar-toggle:visible').click()
-  })
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
+  });
 
   // Offset for Main Navigation
   $('#mainNav').affix({
     offset: {
       top: 100
     }
-  })
+  });
 
   // show listing detail modal when "see more" is clicked
-  $('.listings').on('click', '.see-more', (e) => {
-    showDetails($(e.target).data('mls'))
-  })
+  $('.listings').on('click', '.see-more', function (e) {
+    showDetails($(e.target).data('mls'));
+  });
 
   // submit contact form
-  $('#contact-form').submit(event => {
-    event.preventDefault()
-    const email = $("input#email").val()
-    const data = {
+  $('#contact-form').submit(function (event) {
+    event.preventDefault();
+    var email = $("input#email").val();
+    var data = {
       email: email
-    }
-    submitContact(data)
-      .then(showSubmitSuccess)
-      .fail(showSubmitError)
+    };
+    submitContact(data).then(showSubmitSuccess).fail(showSubmitError);
 
-    $("input#email").val('')
-  })
+    $("input#email").val('');
+  });
 
   // initialize jquery gallery in detail modal
-  $('body').on('shown.bs.modal', '#detail-modal', () => {
-    setUpGallery()
-  })
+  $('body').on('shown.bs.modal', '#detail-modal', function () {
+    setUpGallery();
+  });
 }
 
 //
 // Document Ready
 //
 
-$(function() {
+$(function () {
   // set event handlers
-  handleEvents()
+  handleEvents();
 
   // get data from JSON feed and wait for promise to be returned
-  getListingData(listingPath)
-    .then((data) => {
-      // set global listing object to JSON feed data
-      listingsObj = new ListingData()
+  getListingData(listingPath).then(function (data) {
+    // set global listing object to JSON feed data
+    listingsObj = new ListingData();
 
-      listingsObj.setData(data.feed.entry)
+    listingsObj.setData(data.feed.entry);
 
-      // get template and render
-      getTemplate('js/templates/listings.hbs')
-        .then((template) => {
-          insertTemplate('#listings', template, listingsObj.data)
-        })
-        .fail((err) => console.log('listing template is not available'))
-    })
-    .fail((err) => console.log('data feed is not available'))
-})
+    // get template and render
+    getTemplate('js/templates/listings.hbs').then(function (template) {
+      insertTemplate('#listings', template, listingsObj.data);
+    }).fail(function (err) {
+      return console.log('listing template is not available');
+    });
+  }).fail(function (err) {
+    return console.log('data feed is not available');
+  });
+});
+//# sourceMappingURL=/Users/pliddy/Documents/dev/betterway/scripts.js.map
