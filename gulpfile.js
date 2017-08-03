d9b7300d0985294d3b072ad178c0bfde70da8612
pjliddy <pjliddy@gmail.com>
2017-08-03 11:36:55 -0400

    Gulp serve processes all files
    Serve task is set up to build production files into dist/
    Serve watches source directories and refreshes browser after
    new files are compiled

--- gulpfile.js
+++ gulpfile.js
@@ -26,14 +26,8 @@ const browserSync = require('browser-sync').create()
 // gulp file transfer packages
 // const sftp = require('gulp-sftp')
 
-// unsorted packages
-// const header = require('gulp-header')
-// const pkg = require('./package.json')
-
 // gulp packages for deploying to ftp
 // var gzip = require('gulp-gzip');
-
-
 
 /*
  *  Variable Declarations
@@ -47,6 +41,7 @@ const htmlDest = distDest
 const hbsSrc = 'js/templates/*.hbs'
 const hbsDest = distDest + 'js/templates/'
 
+const sassWatch = 'scss/*.scss'
 const sassSrc = 'scss/styles.scss'
 const sassDest = 'css/'
 const sassOutput = 'styles.css'
@@ -58,11 +53,15 @@ const jsSrc = 'js/**/*.js'
 const jsDest = './dist/js'
 
 const faviconSrc = ['./*.png', './*.ico', './*.svg', './browserconfig.xml', './manifest.json']
+
 const cgiSrc = 'cgi/*.php'
 const cgiDest = distDest + 'cgi/'
+
 const imgSrc = ['img/*.png', 'img/*.jpg', 'img/*.gif']
 const imgDest = distDest + 'img/'
 
+const vendorSrc = 'vendor/**/*'
+const vendorDest = distDest + 'vendor/'
 
 /*
  *  Global Tasks
@@ -72,27 +71,32 @@ const imgDest = distDest + 'img/'
 gulp.task('default', [
   'minify-html',
   'minify-handlebars',
-  // 'sass',
   'minify-css',
   'minify-js',
   'copy-images',
   'copy-cgi',
   'copy-favicons',
-  'deploy-vendor'
+  'copy-vendor'
 ])
 
 // Run development server task with browserSync locally on port 3000
-gulp.task('serve', ['browserSync', 'minify-html',
-'minify-handlebars', 'minify-css', 'minify-js', 'copy-images', 'copy-cgi', 'copy-favicons', 'deploy-vendor'], function() {
+gulp.task('serve', ['browserSync', 'default'], function() {
   gutil.log('Execute: serve')
 
-  gulp.watch(sassSrc, ['sass']);
-  gulp.watch(cssSrc, ['minify-css']);
-  gulp.watch(jsSrc, ['minify-js']);
+  // run gulp tasks when source files change
+  gulp.watch(htmlSrc, ['minify-html'])
+  gulp.watch(hbsSrc, ['minify-handlebars'])
+  gulp.watch(sassWatch, ['minify-css'])
+  gulp.watch(imgSrc, ['copy-images'])
+  gulp.watch(cgiSrc, ['copy-cgi'])
+  gulp.watch(faviconSrc, ['copy-favicons'])
+  gulp.watch(vendorSrc, ['copy-vendor'])
 
-  // Reload the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch(jsSrc, browserSync.reload);
+  // Reload the browser whenever files change
+  gulp.watch(htmlDest, browserSync.reload);
+  gulp.watch(jsDest, browserSync.reload);
+  gulp.watch(vendorDest, browserSync.reload)
+  gulp.watch(cssDest, browserSync.reload)
 });
 
 // Configure the browserSync task
@@ -115,7 +119,7 @@ gulp.task('sass', function() {
   return gulp.src(sassSrc)
   .pipe(sass())
   .pipe(concat(sassOutput))
-  .pipe(changed(sassDest))
+  // .pipe(changed(sassDest))
   .pipe(gulp.dest(sassDest))
 })
 
@@ -128,33 +132,32 @@ gulp.task('minify-css', ['sass'], function() {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(changed(cssDest))
+    // .pipe(changed(cssDest))
     .pipe(gulp.dest(cssDest))
     .pipe(browserSync.reload({
       stream: true
     }))
 })
 
-// Minify html to dist/
-gulp.task('minify-html', function() {
-  return gulp.src(htmlSrc)
+// use minifyHTML to minify html and handlebars files
+function minifyHtmlFiles (src, dest) {
+  return gulp.src(src)
     .pipe(minifyHTML({ conditionals: true, spare:true}))
-    .pipe(changed(htmlDest))
-    .pipe(gulp.dest(htmlDest))
+    // .pipe(changed(dest))
+    .pipe(gulp.dest(dest))
     .pipe(browserSync.reload({
       stream: true
     }))
+}
+
+// Minify html to dist/
+gulp.task('minify-html', function() {
+  return minifyHtmlFiles(htmlSrc, htmlDest)
 })
 
 // Minify handlebars to dist/
 gulp.task('minify-handlebars', function() {
-  return gulp.src(hbsSrc)
-    .pipe(minifyHTML({ conditionals: true, spare:true}))
-    .pipe(changed(hbsDest))
-    .pipe(gulp.dest(hbsDest))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return minifyHtmlFiles(hbsSrc, hbsDest)
 })
 
 // Minify JS to dist/
@@ -165,7 +168,7 @@ gulp.task('minify-js', function() {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(changed(jsDest))
+    // .pipe(changed(jsDest))
     .pipe(gulp.dest(jsDest))
     .pipe(browserSync.reload({
       stream: true
@@ -177,42 +180,39 @@ gulp.task('minify-js', function() {
  */
 
 // Copy favicons to dist/
+function copy (src, dest) {
+  return gulp.src(src)
+    .pipe(changed(dest))
+    .pipe(gulp.dest(dest))
+}
+
 gulp.task('copy-favicons', function() {
-  return gulp.src(faviconSrc)
-    .pipe(changed(distDest))
-    .pipe(gulp.dest(distDest))
+  return copy(faviconSrc, distDest)
 })
 
 // Copy cgi contents to dist/
 gulp.task('copy-cgi', function() {
-  return gulp.src(cgiSrc)
-    .pipe(changed(cgiDest))
-    .pipe(gulp.dest(cgiDest))
+  return copy(cgiSrc,cgiDest)
 })
 
 // Copy images to dist/
 gulp.task('copy-images', function() {
-  return gulp.src(imgSrc)
-    .pipe(changed(imgDest))
-    .pipe(gulp.dest(imgDest))
+  return copy(imgSrc,imgDest)
 })
 
 // Deploy vendor directory to dist/
 gulp.task('deploy-vendor', function() {
-  return gulp.src('vendor/**/*')
-    .pipe(changed(distDest + 'vendor'))
-    .pipe(gulp.dest(distDest + 'vendor'))
+  return copy(vendorSrc, vendorDest)
 })
-//
-// NOT REFACTORED YET
-//
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy-vendor', function() {
   gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+    // .pipe(changed('vendor/bootstrap'))
     .pipe(gulp.dest('vendor/bootstrap'))
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    // .pipe(changed('vendor/jquery'))
     .pipe(gulp.dest('vendor/jquery'))
 
   gulp.src([
@@ -223,10 +223,15 @@ gulp.task('copy-vendor', function() {
       '!node_modules/font-awesome/*.md',
       '!node_modules/font-awesome/*.json'
     ])
+    // .pipe(changed('vendor/font-awesome'))
     .pipe(gulp.dest('vendor/font-awesome'))
+
+  return copy(vendorSrc, vendorDest)
 })
 
-
+//
+// NOT REFACTORED YET
+//
 
 gulp.task('push-dev', function () {
   gutil.log('Execute: push-dev');
@@ -242,51 +247,3 @@ gulp.task('push-dev', function () {
       authFile: '.ftppass'
     }))
 })
-
-
-
-// defining single task with name "build"
-gulp.task('build', function() {
-  gutil.log('Execute: build');
-
-  // only copy desired files to dist folder
-  gulp.src('./css/**').pipe(gulp.dest('./dist/css'));
-  gulp.src('./img/**').pipe(gulp.dest('./dist/img'));
-  gulp.src('./js/**').pipe(gulp.dest('./dist/js'));
-  gulp.src('./vendor/**').pipe(gulp.dest('./dist/vendor'));
-  gulp.src('./cgi/*.php').pipe(gulp.dest('./dist/cgi'));
-  gulp.src('./*.html').pipe(gulp.dest('./dist'));
-  gulp.src('./*.png').pipe(gulp.dest('./dist'));
-  gulp.src('./*.ico').pipe(gulp.dest('./dist'));
-  gulp.src('./*.svg').pipe(gulp.dest('./dist'));
-  gulp.src('./browserconfig.xml').pipe(gulp.dest('./dist'));
-  gulp.src('./manifest.json').pipe(gulp.dest('./dist'));
-  gulp.src('./*.html').pipe(gulp.dest('./dist'));
-
-
-  //minify css
-  // gulp.src('./dist/css/*.css')
-  //   .pipe(minifyCss({compatibility: 'ie8'}))
-  //   .pipe(gulp.dest('./dist'));
-
-  //gzipping css
-  // gulp.src('./dist/css/*.css')
-  //   .pipe(awspublish.gzip({ ext: '.gz' }))
-  //   .pipe(gulp.dest('./dist'));
-
-  //minifying html
-  // gulp.src('./dist/*.html')
-  //   .pipe(minifyHTML({ conditionals: true, spare:true}))
-  //   .pipe(gulp.dest('./dist'));
-
-  // var headers = { 'Cache-Control': 'max-age=315360000, no-transform, public' };
-
-});
-
-// Set the banner content
-// var banner = ['/*!\n',
-//   ' * Betterway Atlanta Realty - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-//   ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-//   ' */\n',
-//   ''
-// ].join('');
