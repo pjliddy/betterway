c3f8224f2ceec10c4f51d5dd2760f72a4864cee7
pjliddy <pjliddy@gmail.com>
2017-08-05 14:41:40 -0400

    Added dist files

---
+++ dist/cgi/submit.php
@@ -0,0 +1,27 @@
+<?php
+  if(empty($_POST['email'])     ||
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    {
+     echo "Form field(s) are empty";
+     return false;
+   } else {
+     $contact_email = strip_tags(htmlspecialchars($_POST['email']));
+     $name = strip_tags(htmlspecialchars($_POST['name']));
+     $phone = strip_tags(htmlspecialchars($_POST['phone']));
+
+     $to = "dan@bwarealty.com";
+     $bcc = "pjliddy@gmail.com";
+     $subject = "Contact from bwarealty.com";
+     $message = "I'm interested in listing my house. Please contact me.\n\n" .
+                "Name: " . $name . "\n" .
+                "Email Address: " . $contact_email . "\n" .
+                "Phone: " . $phone;
+     $from = "contact@bwarealty.com";
+     $headers = "From:" . $from . "\r\n" .
+                "Reply-To: " . $contact_email . "\r\n" .
+                "Bcc: $bcc";
+     mail($to,$subject,$message,$headers);
+     echo "Mail Sent.";
+     return true;
+   }
+?>
